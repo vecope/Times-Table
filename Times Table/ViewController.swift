1084486837a80c2ce2d8735e40b404b08aa17189
CAMILO ALEJANDRO ALFONSO SANCHEZ <ca.alfonso967@isc331.uniandes.edu.co>
2017-10-18 15:03:37 -0500

    Initial Commit

---
+++ Times Table/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Times Table
+//
+//  Created by CAMILO ALEJANDRO ALFONSO SANCHEZ on 10/18/17.
+//  Copyright © 2017 CAMILO ALEJANDRO ALFONSO SANCHEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
