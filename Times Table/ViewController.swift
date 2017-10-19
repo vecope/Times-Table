f3a6d579756866b25b1569c6ada7531156a7aff1
Camilo Alfonso <6633004+vecope@users.noreply.github.com>
2017-10-19 14:19:48 -0500

    Basic project
    Combines slider and table view in a single view app

--- Times Table/ViewController.swift
+++ Times Table/ViewController.swift
@@ -8,8 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var table: UITableView!
+    
+   
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = String((indexPath.row+1) * Int(slider.value))
+        return cell
+    }
 
+    
+    @IBAction func sliderChanged(_ sender: Any) {
+
+        print(Int(slider.value))
+        table.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
